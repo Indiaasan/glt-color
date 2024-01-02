4fef6f2b619d84962fbaeae765d56364b276cfbd
Indiaasan <150584042+Indiaasan@users.noreply.github.com>
2024-01-02 18:21:23 +1200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 # 设置各变量
-UUID='624b03d6-877d-4d86-9ae8-eb35db01e975'
-VMESS_WSPATH='/vmess-624b03d6'
-VLESS_WSPATH='/vless-624b03d6'
-TROJAN_WSPATH='/trojan-624b03d6'
-SS_WSPATH='/shadowsocks-624b03d6'
+UUID='894df7ef-157c-4540-b58d-6703dcc1cb2b'
+VMESS_WSPATH='/yydpz-vmess'
+VLESS_WSPATH='/yydpz-vless'
+TROJAN_WSPATH='/yydpz-trojan'
+SS_WSPATH='/yydpz-shadowsocks'
 
 # 安装系统依赖
 check_dependencies() {
